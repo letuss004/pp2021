96b5053f78fbec3375e298084ff3e66b3dba2184
Anh Tu Le <74813352+letuss004@users.noreply.github.com>
2021-04-04 17:40:30 +0700

    Add files via upload

---
+++ 2/student/mark/oop/py/work1.py
@@ -0,0 +1,162 @@
+"""
+:author: Le Anh Tu
+:ID: Ba9-067
+:Lab work: 1
+"""
+# --------lists-----------
+studentName = []
+studentID = []
+studentDoB = []
+courseID = []
+courseName = []
+studentInformation = {}
+
+
+# ------------------------------ menu -------------------------------------
+def menu_option():
+    print("1. Set information for student: ")
+    print("2. Set information for course: ")
+    print("3. Set mark for student by course: ")
+    print("4. Get information for student: ")
+    print("5. Get information for course: ")
+    print("6. Get information for student course management: ")
+    print("0. Quit.")
+
+
+# ------------------------------ function's option -------------------------------------
+def input_domain(value, min_value, max_value):
+    while value < min_value or value > max_value:
+        value = int(input("Your input is invalid! Enter again: "))
+    return value
+
+
+def set_inf_student():
+    global i
+    student_number = int(input("Enter number of student: "))
+    student_number = input_domain(student_number, 1, 999999)
+    # --------list-----------
+    for i in range(0, student_number):
+        print("The order of student in class: %d ------------------------------" % (i + 1))
+        a = str(input("Enter student ID: "))
+        b = str(input("Enter student name: "))
+        c = str(input("Enter student DoB: "))
+        studentID.append(a)
+        studentName.append(b)
+        studentDoB.append(c)
+
+
+def set_inf_course():
+    global i
+
+    courses_number = int(input("Enter number of courses: "))
+    courses_number = input_domain(courses_number, 0, 999999)
+    # --------list-----------
+    for i in range(0, courses_number):
+        print("The order of course in courses: %d ------------------------------" % (i + 1))
+        a = str(input("Enter course name: "))
+        b = str(input("Enter course ID: "))
+        courseName.append(a)
+        courseID.append(b)
+
+
+def get_st_inf():
+    global i
+    for i in range(0, len(studentName)):
+        print("This is the information of " + studentName[i] + ": ")
+        print("student name is: " + studentName[i])
+        print("student id is: " + studentID[i])
+        print("student DoB is: " + studentDoB[i])
+
+
+def get_cour_inf():
+    global i
+    for i in range(0, len(courseName)):
+        print("This is the information of " + courseName[i] + ": ")
+        print("This is name of course: " + courseName[i])
+        print("This is id of course: " + courseID[i])
+
+
+def cond_quit(input_value, min_value, max_value):
+    if min_value < input_value < max_value:
+        a = True
+    else:
+        a = False
+        print("You choose quit! Thank for using my program.")
+    return a
+
+
+# ------------------------------ menu option -------------------------------------
+print("Program started !!!")
+menu_option()
+choice = int(input("Enter your choice: "))
+
+while choice != 0:
+    break_point = False
+    if choice == 1:
+        set_inf_student()
+
+        print("---------------------End of this choice---------------------")
+        menu_option()
+        choice = int(input("Do u want to get more choice: "))
+        if not cond_quit(choice, 0, 6):
+            break
+
+    elif choice == 2:
+        min_op = 0
+        max_op = 6
+        set_inf_course()
+
+        print("---------------------End of this choice---------------------")
+        menu_option()
+        choice = int(input("Do u want to get more choice: "))
+        if not cond_quit(choice, 0, 6):
+            break
+
+    elif choice == 3:
+        name = str(input("Enter name of student u want to manage: "))
+        st_cour_mang = {}
+        check_stop = False
+        for i in range(0, len(studentName)):
+            if studentName[i] == name:
+                check_stop = True
+                stcourse = int(input("How many course " + studentName[i] + " is learning: "))
+                while stcourse > len(courseName) or stcourse < 0:
+                    print("course amount is run out of course number you entered! Enter again: ")
+                    stcourse = int(input("How many course " + studentName[i] + " is learning: "))
+                for j in range(0, stcourse):
+                    checka = False
+                    name_course_curr = str(input("Enter course name to manage: "))
+                    for k in range(0, len(courseName)):
+                        if name_course_curr == courseName[k]:
+                            checka = True
+                            mark = int(input("Enter mark of " + name_course_curr + ": "))
+                            st_cour_mang[name_course_curr] = mark
+                            studentInformation[name] = st_cour_mang
+                            break
+                    if not check_stop or not checka:
+                        print("----------------- course name input not found!----------------- ")
+                        if not check_stop:
+                            exit(100)
+        if not check_stop:
+            print("----------------- student name input not found!----------------- ")
+            exit(100)
+        print("---------------------End of this choice---------------------")
+        menu_option()
+        choice = int(input("Do u want to get more choice: "))
+        if 0 > choice > 6:
+            print("You choose quit! Thank for using my program.")
+            break
+    elif choice == 4:
+        get_st_inf()
+
+        choice = int(input("Do u want to get more choice: "))
+    elif choice == 5:
+        get_cour_inf()
+
+        choice = int(input("Do u want to get more choice: "))
+    elif choice == 6:
+        print(studentInformation)
+        choice = int(input("Do u want to get more choice: "))
+    else:
+        print("You choose quit! Thank for using my program.")
+        break
---
+++ 2/student/mark/oop/py/work2.py
@@ -0,0 +1,203 @@
+class Student:
+    def __init__(self, name, s_id, dob):
+        self._stdName = name
+        self._stdID = s_id
+        self._stdDoB = dob
+
+    def set_st_name(self, name):
+        self._stdName = name
+
+    def set_st_id(self, std_id):
+        self._stdID = std_id
+
+    def set_st_dob(self, dob):
+        self._stdDoB = dob
+
+    def get_st_name(self):
+        return self._stdName
+
+    def get_st_id(self):
+        return self._stdID
+
+    def get_st_dob(self):
+        return self._stdDoB
+
+
+class Course:
+    def __init__(self, name, c_id):
+        self._name = name
+        self._id = c_id
+
+    def set_c_name(self, name):
+        self._name = name
+
+    def set_c_id(self, c_id):
+        self._id = c_id
+
+    def get_name(self):
+        return self._name
+
+    def get_id(self):
+        return self._id
+
+
+class SMM:
+    _student_name = ""
+    _course_inf = {}
+
+    def __init__(self):
+        pass
+
+    def set_inf(self, name, course_inf):
+        self._student_name = name
+        self._course_inf = course_inf
+
+    def set_st_name(self, student_name):
+        self._student_name = student_name
+
+    def get_st_name(self):
+        return self._student_name
+
+    def set_crs_inf(self, dic_inf):
+        self._course_inf = dic_inf
+
+    def get_crs_inf(self):
+        return self._course_inf
+
+
+# ------------------------------ database -------------------------------------
+st_list = [Student("#", "#", "#")]
+course_list = [Course("#", "#")]
+smm_list = [SMM()]
+
+st_list.clear()
+course_list.clear()
+smm_list.clear()
+
+count_smm = 0
+# ------------------------------ method -------------------------------------
+
+
+def input_domain(value, min_value, max_value):
+    while value < min_value or value > max_value:
+        value = int(input("Your input is invalid! Enter again: "))
+    return value
+
+
+def quit_menu():
+    global choice
+    print("---------------------End of this choice---------------------")
+    choice = int(input("Make other choice, 0 to quit: "))
+    choice = input_domain(choice, 0, 6)
+    if choice == 0:
+        print("You choose quit! Thank for using my program.")
+
+
+def choice1():
+    global i
+    num_of_st = int(input("Enter number of student in the class: "))
+    for i in range(0, num_of_st):
+        name = str(input("Enter student name: "))
+        s_id = str(input("Enter student id: "))
+        dob = str(input("Enter student dob: "))
+        st_list.append(Student(name, s_id, dob))
+
+
+def choice2():
+    global i
+    num_of_cour = int(input("Enter number of course: "))
+    for i in range(0, num_of_cour):
+        c_name = str(input("Enter course name: "))
+        c_id = str(input("Enter course id: "))
+        course_list.append(Course(c_name, c_id))
+
+
+def choice3():
+    global choice, i, count_smm
+    name = str(input("Enter name of student want to manage: "))
+    cour_inf_lit = {}
+    if checkStList(name):
+        smm_list.append(SMM())
+        smm_list[count_smm].set_st_name(name)
+        num_of_cour = int(input("Enter number of courses this student studied: "))
+        for i in range(0, num_of_cour):
+            c_name = input("Enter name of course: ")
+            if checkCourList(c_name):
+                mark = int(input("Enter mark of this course: "))
+                cour_inf_lit[c_name] = mark
+                smm_list[count_smm].set_crs_inf(cour_inf_lit)
+            else:
+                print("Input course did not import.")
+        count_smm = count_smm + 1
+    else:
+        choice = int(input("Input wrong ! Press 3 to do again, 0 to quit: !"))
+
+
+def checkStList(name):
+    global i
+
+    check = False
+    for i in st_list:
+        if name == i.get_st_name():
+            check = True
+    return check
+
+
+def checkCourList(c_name):
+    global i
+    for i in course_list:
+        if i.get_name() == c_name:
+            return True
+
+
+# ------------------------------ menu -------------------------------------
+def menu_option():
+    print("1. Set information for student: ")
+    print("2. Set information for course: ")
+    print("3. Set mark for student by course: ")
+    print("4. Get information for student: ")
+    print("5. Get information for course: ")
+    print("6. Get information for student course management: ")
+    print("0. Quit.")
+
+
+print("Program started !!!")
+menu_option()
+choice = int(input("Enter your choice: "))
+
+while choice != 0:
+    if choice == 1:
+        choice1()
+        quit_menu()
+
+    elif choice == 2:
+        choice2()
+        quit_menu()
+
+    elif choice == 3:
+        choice3()
+        quit_menu()
+
+    elif choice == 4:
+        for i in st_list:
+            print("Student's name = " + i.get_st_name(), end=". ID = ")
+            print(i.get_st_id(), end=". DoB = " + i.get_st_dob())
+            print()
+        quit_menu()
+
+    elif choice == 5:
+        print("This is all information of course: ")
+        for i in course_list:
+            print("Course's name = " + i.get_name(), end=". ID = ")
+            print(i.get_id())
+        quit_menu()
+
+    elif choice == 6:
+        for i in smm_list:
+            print("This is mark information of " + i.get_st_name() + ": ", end="")
+            print(i.get_crs_inf())
+        quit_menu()
+
+    else:
+        print("You choose quit! Thank for using my program.")
+        break
